3703e2b89cb1feb45f5e00cb1ec977a98631f054
1u5t1n14n <177038973+1u5t1n14n@users.noreply.github.com>
2024-12-05 16:09:21 +0100

    Make Home Manager Great

--- flake.nix
+++ flake.nix
@@ -23,7 +23,7 @@
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.users."1u5t1n14n" = import ./home/home.nix;
-            home-manager.backupFileExtension = "HMbackup";
+            home-manager.backupFileExtension = "back";
           }
         ];
       };
