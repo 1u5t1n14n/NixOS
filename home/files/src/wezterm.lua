aa8b57417b4e265ce3ea168b1fcf4db8a920b798
1u5t1n14n <177038973+1u5t1n14n@users.noreply.github.com>
2024-12-05 10:52:26 +0100

    Update WezTerm Config

--- home/files/src/wezterm.lua
+++ home/files/src/wezterm.lua
@@ -30,12 +30,5 @@ config.window_background_opacity = 0.9
 config.text_background_opacity = 1.0
 
 config.color_scheme = "Kanagawa (Gogh)"
---config.window_decorations = "NONE" (is kinda not working)
 
---local mux = wezterm.mux
---wezterm.on("gui-startup", function(window)
-	--local tab, pane, window = mux.spawn_window(cmd or {})
-	--local gui_window = window:gui_window()
-	--gui_window:maximize()
---end)
 return config
