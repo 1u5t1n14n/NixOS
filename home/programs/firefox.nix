54a55ede2b778c5a0ff785ac49f55970382b642d
1u5t1n14n <177038973+1u5t1n14n@users.noreply.github.com>
2024-12-05 15:33:29 +0100

    Add more Engines to Firefox

--- home/programs/firefox.nix
+++ home/programs/firefox.nix
@@ -2,54 +2,90 @@
 
 {
 
-  programs.firefox.enable = true;
-  programs.firefox.profiles.Firefox = {
-    name = "Firefox";
-    isDefault = true;
-    id = 0;
-    search = {
-      force = true;
-      default = "SearXNG";
-      engines = {
-        "Google".metaData.alias = "@g";
-        "NixOS Wiki" = {
-          urls = [{ template = "https://wiki.nixos.org/index.php?search={searchTerms}"; }];
-          iconUpdateURL = "https://search.nixos.org/images/nix-logo.png";
-          updateInterval = 7 * 24 * 60 * 60 * 1000;
-          definedAliases = [ "@nw" ];
-        };
-        "Reverse Dictionary" = {
-          urls = [{ template = "https://www.onelook.com/reverse-dictionary.shtml?s={searchTerms}"; }];
-          iconUpdateURL = "https://external-content.duckduckgo.com/ip3/www.onelook.com.ico";
-          updateInterval = 7 * 24 * 60 * 60 * 1000;
-          definedAliases = [ "@rw" ];
-        };
-        "Pexels" = {
-          urls = [{ template = "https://www.pexels.com/search/{searchTerms}"; }];
-          iconUpdateURL = "https://external-content.duckduckgo.com/ip3/www.pexels.com.ico";
-          updateInterval = 7 * 24 * 60 * 60 * 1000;
-          definedAliases = [ "@px" ];
-        };
+	programs.firefox.enable = true;
+	programs.firefox.profiles.Firefox = {
+		name = "Firefox";
+		isDefault = true;
+		id = 0;
+		search = {
+			force = true;
+			default = "SearXNG";
+			engines = {
+				"Google".metaData.alias = "@g";
+				"NixOS Wiki" = {
+					urls = [{ template = "https://wiki.nixos.org/index.php?search={searchTerms}"; }];
+					iconUpdateURL = "https://wiki.nixos.org/favicon.ico";
+					updateInterval = 7 * 24 * 60 * 60 * 1000;
+					definedAliases = [ "@nw" ];
+				};
+				"Startpage" = {
+					urls = [{ template = "https://www.startpage.com/sp/search?query={searchTerms}"; }];
+					iconUpdateURL = "https://www.startpage.com/favicon.ico";
+					updateInterval = 7 * 24 * 60 * 60 * 1000;
+					definedAliases = [ "@sp" ];
+				};
+				"Qwant" = {
+					urls = [{ template = "https://www.qwant.com/?q={searchTerms}"; }];
+					iconUpdateURL = "https://www.qwant.com/favicon.ico";
+					updateInterval = 7 * 24 * 60 * 60 * 1000;
+					definedAliases = [ "@qw" ];
+				};
+				"Presearch" = {
+					urls = [{ template = "https://presearch.com/search?q={searchTerms}"; }];
+					iconUpdateURL = "https://www.presearch.com/favicon.ico";
+					updateInterval = 7 * 24 * 60 * 60 * 1000;
+					definedAliases = [ "@ps" ];
+				};
+				"Brave" = {
+					urls = [{ template = "https://search.brave.com/search?q={searchTerms}"; }];
+					iconUpdateURL = "https://www.brave.com/favicon.ico";
+					updateInterval = 7 * 24 * 60 * 60 * 1000;
+					definedAliases = [ "@br" ];
+				};
+				"Reverse Dictionary" = {
+					urls = [{ template = "https://www.onelook.com/reverse-dictionary.shtml?s={searchTerms}"; }];
+					iconUpdateURL = "https://onelook.com/favicon.ico";
+					updateInterval = 7 * 24 * 60 * 60 * 1000;
+					definedAliases = [ "@rw" ];
+				};
+				"Pexels" = {
+					urls = [{ template = "https://www.pexels.com/search/{searchTerms}"; }];
+					iconUpdateURL = "https://www.pexels.com/favicon.ico";
+					updateInterval = 7 * 24 * 60 * 60 * 1000;
+					definedAliases = [ "@px" ];
+				};
 				"NixOS Packages" = {
-          urls = [{ template = "https://search.nixos.org/packages?type=packages&query={searchTerms}"; }];
-          iconUpdateURL = "https://search.nixos.org/images/nix-logo.png";
-          updateInterval = 7 * 24 * 60 * 60 * 1000;
-          definedAliases = [ "@np" ];
-        };
-        "SearXNG" = {
-          urls = [{ template = "http://localhost/search?q={searchTerms}"; }];
-          iconUpdateURL = "https://baresearch.org/favicon.ico";
-          updateInterval = 7 * 24 * 60 * 60 * 1000;
-          definedAliases = [ "@sx" ];
-        };
-        "YouTube" = {
-          urls = [{ template = "https://www.youtube.com/results?search_query={searchTerms}"; }];
-          iconUpdateURL = "https://duckduckgo.com/assets/icons/favicons/youtube.png";
-          updateInterval = 7 * 24 * 60 * 60 * 1000;
-          definedAliases = [ "@yt" ];
-        };
-      };
-    };
-  };
+					urls = [{ template = "https://search.nixos.org/packages?type=packages&query={searchTerms}"; }];
+					iconUpdateURL = "https://wiki.nixos.org/favicon.ico";
+					updateInterval = 7 * 24 * 60 * 60 * 1000;
+					definedAliases = [ "@np" ];
+				};
+				"SearXNG" = {
+					urls = [{ template = "http://localhost/search?q={searchTerms}"; }];
+					iconUpdateURL = "https://baresearch.org/favicon.ico";
+					updateInterval = 7 * 24 * 60 * 60 * 1000;
+					definedAliases = [ "@sx" ];
+				};
+				"YouTube" = {
+					urls = [{ template = "https://www.youtube.com/results?search_query={searchTerms}"; }];
+					iconUpdateURL = "https://www.youtube.com/favicon.ico";
+					updateInterval = 7 * 24 * 60 * 60 * 1000;
+					definedAliases = [ "@yt" ];
+				};
+				"Perplexity" = {
+					urls = [{ template = "https://www.perplexity.ai/search/{searchTerms}"; }];
+					iconUpdateURL = "https://www.perplexity.ai/favicon.ico";
+					updateInterval = 7 * 24 * 60 * 60 * 1000;
+					definedAliases = [ "@pp" ];
+				};
+				"Arch Wiki" = {
+					urls = [{ template = "https://wiki.archlinux.org/title/Special:Search/{searchTerms}"; }];
+					iconUpdateURL = "https://wiki.archlinux.org/favicon.ico";
+					updateInterval = 7 * 24 * 60 * 60 * 1000;
+					definedAliases = [ "@aw" ];
+				};
+			};
+		};
+	};
 
 }
