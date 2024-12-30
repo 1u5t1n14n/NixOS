29e528f7f58489d4592fed9a34e850e30e4585b3
1u5t1n14n <177038973+1u5t1n14n@users.noreply.github.com>
2024-12-30 17:52:18 +0100

    Remove Conceal and preview for VimTeX

--- home/files/init.lua
+++ home/files/init.lua
@@ -287,26 +287,8 @@ dashboard.section.header.val = {
 }
 alpha.setup(dashboard.opts)
 -- Vimtex
-vim.g.vimtex_view_method = "zathura"
 vim.g.vimtex_format_enabled = 1
 vim.g.vimtex_compiler_method = "latexmk"
-vim.opt.conceallevel = 2
-vim.opt.concealcursor = "nc"
-vim.g.vimtex_syntax_conceal = {
-	accents = 1,
-	ligatures = 1,
-	cites = 1,
-	fancy = 1,
-	spacing = 1,
-	greek = 1,
-	math_bounds = 1,
-	math_delimiters = 1,
-	math_fracs = 1,
-	math_super_sub = 1,
-	math_symbols = 1,
-	sections = 1,
-	styles = 1,
-}
 require("markview")
 -- LSP
 require("autoclose").setup()
