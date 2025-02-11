89f977a9204ea0a0966bc71c45725f5d68d44671
1u5t1n14n <177038973+1u5t1n14n@users.noreply.github.com>
2025-02-11 07:33:56 +0100

    Add user For Kian

--- Shared/Services/users.nix
+++ Shared/Services/users.nix
@@ -11,6 +11,16 @@
 		extraGroups = [ "networkmanager" "wheel" ];
 		packages = with pkgs; [ ];
 	};
+	users.users.kian = {
+		isNormalUser = true;
+		createHome = true;
+		initialHashedPassword = "$y$j9T$OPBgQ9p/.wQp/w73CWohx/$aochBGA4fXfdeifRAsPhSYPNhfprwJXsE5wwmuneQT7";
+		description = "Kians User";
+		shell = pkgs.zsh;
+		extraGroups = [ "networkmanager" ];
+		packages = with pkgs; [ ];
+	};
+
 
 	security.sudo = {
 		enable = true;
