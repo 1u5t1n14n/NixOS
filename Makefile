fa9fd620b474061766c66d863d147693b775137a
1u5t1n14n <177038973+1u5t1n14n@users.noreply.github.com>
2025-03-21 18:26:54 +0100

    Be unable to do the thing I said in the last Commit so it is just the status quo

--- Makefile
+++ Makefile
@@ -4,5 +4,4 @@ nixosRebuild:
 	@sudo $$(cat .env) nixos-rebuild switch --impure --flake .\#$(HOST)
 
 homeManager:
-	@export USER=$(USER)
 	@home-manager switch --flake .\#$(USER) -b hm-backup
--- Shared/Home/git.nix
+++ Shared/Home/git.nix
@@ -4,8 +4,8 @@
 
 	programs.git = {
 		enable = true;
-		userName = "${builtins.getEnv "USER"}";
-		userEmail = "${builtins.getEnv "GITEMAIL"}";
+		userName = "1u5t1n14n";
+		userEmail = "177038973+1u5t1n14n@users.noreply.github.com";
 	};
 
 }
