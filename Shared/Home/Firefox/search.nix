8047711585f70792ce9abe9a6e280a70c721466f
1u5t1n14n <177038973+1u5t1n14n@users.noreply.github.com>
2025-03-26 15:29:00 +0100

    Set Update Interval directly

--- Shared/Home/Firefox/search.nix
+++ Shared/Home/Firefox/search.nix
@@ -1,14 +1,11 @@
 { ... }:
 
-let
-	updateInterval = 4 * 7 * 24 * 60 * 60 * 1000;
-
-in {
+{
 
 	programs.firefox.profiles.default.search = {
 		default = "ddg";
 		engines = {
-			nixPackages = {
+			"Nix Packages" = {
 				name = "Nix Packages";
 				urls = [{
 					template = "https://search.nixos.org/packages";
@@ -18,22 +15,22 @@ in {
 					];
 				}];
 				iconUpdateURL = "https://wiki.nixos.org/favicon.ico";
-				updateInterval = updateInterval;
+				updateInterval = 24 * 60 * 60 * 1000;
 				definedAliases = [ "@np" ];
 			};
-			nixosWiki = {
+			"NixOS Wiki" = {
 				name = "NixOS Wiki";
 				urls = [{ template = "https://wiki.nixos.org/w/index.php?search={searchTerms}"; }];
 				iconUpdateURL = "https://wiki.nixos.org/favicon.ico";
-				updateInterval = updateInterval;
+				updateInterval = 24 * 60 * 60 * 1000;
 				definedAliases = [ "@nw" ];
 			};
-			youTube = {
+			"YouTube" = {
 				name = "YouTube";
 				urls = [{ template = "https://www.youtube.com/results?search_query={searchTerms}"; }];
 				iconUpdateURL = "https://youtube.com/favicon.ico";
-				updateInterval = updateInterval;
-				definedAliases = [ "@nw" ];
+				updateInterval = 24 * 60 * 60 * 1000;
+				definedAliases = [ "@yt" ];
 			};
 
 			"Bing".metaData.hidden = true;
