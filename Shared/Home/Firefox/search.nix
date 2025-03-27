5c09b090c6eba8487a129c18ac8d5da93d97a854
1u5t1n14n <177038973+1u5t1n14n@users.noreply.github.com>
2025-03-27 20:07:06 +0100

    Add Firefox Search Engine

--- Shared/Home/Firefox/search.nix
+++ Shared/Home/Firefox/search.nix
@@ -3,40 +3,36 @@
 {
 
 	programs.firefox.profiles.default.search = {
-		default = "ddg";
+		default = "DuckDuckGo";
 		engines = {
 			"Nix Packages" = {
 				name = "Nix Packages";
-				urls = [{
-					template = "https://search.nixos.org/packages";
-					params = [
-						{ name = "type"; value = "packages"; }
-						{ name = "query"; value = "{searchTerms}"; }
-					];
-				}];
-				iconUpdateURL = "https://wiki.nixos.org/favicon.ico";
-				updateInterval = 24 * 60 * 60 * 1000;
+				urls = [{ template = "https://search.nixos.org/packages"; }];
+				iconMapObj."16" = "https://wiki.nixos.org/favicon.ico";
 				definedAliases = [ "@np" ];
+			};
+			"NixOS Options" = {
+				name = "NixOS Options";
+				urls = [{ template = "https://search.nixos.org/options?channel=unstable&query={searchTerms}"; }];
+				iconMapObj."16" = "https://wiki.nixos.org/favicon.ico";
+				definedAliases = [ "@no" ];
 			};
 			"NixOS Wiki" = {
 				name = "NixOS Wiki";
 				urls = [{ template = "https://wiki.nixos.org/w/index.php?search={searchTerms}"; }];
-				iconUpdateURL = "https://wiki.nixos.org/favicon.ico";
-				updateInterval = 24 * 60 * 60 * 1000;
+				iconMapObj."16" = "https://wiki.nixos.org/favicon.ico";
 				definedAliases = [ "@nw" ];
 			};
 			"YouTube" = {
 				name = "YouTube";
 				urls = [{ template = "https://www.youtube.com/results?search_query={searchTerms}"; }];
-				iconUpdateURL = "https://youtube.com/favicon.ico";
-				updateInterval = 24 * 60 * 60 * 1000;
+				iconMapObj."16" = "https://youtube.com/favicon.ico";
 				definedAliases = [ "@yt" ];
 			};
 
 			"Bing".metaData.hidden = true;
 			"Google".metaData.hidden = true;
 			"DuckDuckGo".metaData.alias = "@d";
-			"Wikipedia (en)".metaData.alias = "@w";
 		};
 	};
 
