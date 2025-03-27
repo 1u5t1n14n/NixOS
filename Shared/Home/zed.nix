893de766aa7d6a3db1f2ed1584c3500882dd711c
1u5t1n14n <177038973+1u5t1n14n@users.noreply.github.com>
2025-03-27 20:43:41 +0100

    Update Zed Preferences

--- Shared/Home/zed.nix
+++ Shared/Home/zed.nix
@@ -8,18 +8,31 @@
 		userSettings = {
 			vim_mode = true;
 			features.copilot = true;
-			telemetry.metrics = false;
+			telemetry = {
+				metrics = false;
+				diagnostics = false;
+			};
 			theme = {
 				mode = "dark";
 				dark = "Andromeda";
+				light = "Gruvbox Light Soft";
 			};
 			active_pane_modifiers.border_size = 0.0;
 			autosave = "off";
 			restore_on_startup = "none";
-			buffer_font_family = "JetBrains Mono";
+			buffer_font_family = "JetBrainsMono";
+			terminal.font_family = "JetBrainsMono";
+			ui_font_family = "JetBrainsMono";
+			dock = "right";
 			cursor_blink = false;
 			cursor_shape = "block";
 			tab_bar.show = false;
+			toolbar = {
+				breadcrumbs = false;
+				quick_actions = false;
+			};
+			always_treat_brackets_as_autoclosed = true;
+			hour_format = "hour24";
 		};
 	};
 
