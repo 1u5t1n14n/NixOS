7a847e18b9e55d1634bf2fc39cfe0d1612d63c83
1u5t1n14n <177038973+1u5t1n14n@users.noreply.github.com>
2025-04-23 17:39:38 +0200

    Add Keys for Hyperion

--- Shared/Secrets/sops.nix
+++ Shared/Secrets/sops.nix
@@ -4,31 +4,28 @@
 
 	imports = [ inputs.sops.nixosModules.sops ];
 
-	sops.defaultSopsFile = ./Secrets.yaml;
-	sops.defaultSopsFormat = "yaml";
-
-	#sops.age.keyFile = "/etc/sops/age/key.txt";
-	sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
-	sops.age.generateKey = true;
-
-	sops.validateSopsFiles = true;
-
-	sops.secrets."user/kian" = {
-		neededForUsers = true;
-	};
-	sops.secrets."user/root" = {
-		neededForUsers = true;
-	};
-	sops.secrets."user/user" = {
-		neededForUsers = true;
-	};
-	sops.secrets."services/paperless" = {
-		owner = lib.mkIf (!hasDesktop)
-		config.services.paperless.user;
-	};
-	sops.secrets."services/nextcloud" = {
-		owner = lib.mkIf (!hasDesktop)
-		config.services.nextcloud.config.dbuser;
+	sops = {
+		defaultSopsFile = ./Secrets.yaml;
+		defaultSopsFormat = "yaml";
+		age = {
+			keyFile = "/etc/sops/age/key.txt";
+			sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+			generateKey = true;
+		};
+		validateSopsFiles = true;
+		secrets = {
+			"user/kian".neededForUsers = true;
+			"user/root".neededForUsers = true;
+			"user/user".neededForUsers = true;
+			"services/paperless" = {
+				owner = lib.mkIf (!hasDesktop)
+					config.services.paperless.user;
+			};
+			"services/nextcloud" = {
+				owner = lib.mkIf (!hasDesktop)
+					config.services.nextcloud.config.dbuser;
+			};
+		};
 	};
 
 }
