8bde32371e9ebd7916a906f14c5bd04e020e9990
1u5t1n14n <177038973+1u5t1n14n@users.noreply.github.com>
2025-04-24 11:19:19 +0200

    Make Activation Script export private Key

--- Shared/Secrets/sops.nix
+++ Shared/Secrets/sops.nix
@@ -1,6 +1,10 @@
 { inputs, userName, config, hasDesktop, lib, pkgs, ... }:
 
-{
+let
+	user = config.users.users.${userName};
+	sshKeyFile = "/etc/ssh/ssh_host_ed25519_key";
+
+in {
 
 	imports = [ inputs.sops.nixosModules.sops ];
 	environment.systemPackages = [ pkgs.ssh-to-age pkgs.sops ];
@@ -10,10 +14,6 @@
 		defaultSopsFormat = "yaml";
 		age = {
 			keyFile = "/etc/sops/age/keys.txt";
-			# Somehow does not provide the correct key.
-			# You are better off just ssh-to-age'ing
-			# the keys for yourself.
-
 			sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
 			generateKey = true;
 		};
@@ -33,9 +33,10 @@
 		};
 	};
 
-	system.activationScripts.exportAgeKey = ''
-		mkdir -p ${config.users.users.${userName}.home}/.config/sops/age/
-		$(cat /etc/ssh/ssh_host_ed25519_key.pub | ${pkgs.ssh-to-age}/bin/ssh-to-age) > ${config.users.users.${userName}.home}/.config/sops/age/keys.txt
+	system.activationScripts.copyToHomeDir = ''
+		mkdir -p ${user.home}/.config/sops/age/
+		${pkgs.ssh-to-age}/bin/ssh-to-age -private-key -i ${sshKeyFile} -o ${user.home}/.config/sops/age/keys.txt
+		chown -R ${userName}:${user.group} ${user.home}/.config/sops/age/keys.txt
 	'';
 
 }
