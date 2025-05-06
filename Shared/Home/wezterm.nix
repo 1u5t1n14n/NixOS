e994fe4198db668cbc9ac7a48220931232ceb3a7
1u5t1n14n <177038973+1u5t1n14n@users.noreply.github.com>
2025-05-06 15:59:02 +0200

    Update WezTerms Config

--- Shared/Home/wezterm.nix
+++ Shared/Home/wezterm.nix
@@ -5,6 +5,18 @@
 	programs.wezterm = {
 		enable = true;
 		enableZshIntegration = true;
+		extraConfig = ''
+			return {
+				hide_tab_bar_if_only_one_tab = true,
+				window_background_opacity = 0.9,
+				window_padding = {
+					left = 0,
+					right = 0,
+					top = 0,
+					bottom = 0,
+				},
+			}
+		'';
 	};
 
 }
