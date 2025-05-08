1fada8ed632537314c774b4a53c5b068c0fad4df
1u5t1n14n <177038973+1u5t1n14n@users.noreply.github.com>
2025-05-08 13:21:02 +0200

    Add more Inline Hints to Firefox

--- Shared/Home/Firefox/style.nix
+++ Shared/Home/Firefox/style.nix
@@ -3,10 +3,21 @@
 {
 
 	programs.firefox.profiles.default.userContent = ''
-		/* add '[pdf]' next to links to PDF files */
 		a[href$=".pdf"]:after {
 			font-size: smaller;
 			content: " [pdf]";
+		}
+		a[href$=".mp4"]:after {
+			font-size: smaller;
+			content: " [mp4]";
+		}
+		a[href$=".webp"]:after {
+			font-size: smaller;
+			content: " [webp]";
+		}
+		a[href$=".zip"]:after {
+			font-size: smaller;
+			content: " [zip]";
 		}
 	'';
 
