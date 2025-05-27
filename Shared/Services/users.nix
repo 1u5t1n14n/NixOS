8a6a6e41fabda9af284436514d1071c130b8abda
1u5t1n14n <177038973+1u5t1n14n@users.noreply.github.com>
2025-05-27 19:43:48 +0200

    Make Users dependent on Sops Secret Options

--- Shared/Services/users.nix
+++ Shared/Services/users.nix
@@ -5,15 +5,15 @@
 	users.users.${userName} = {
 		isNormalUser = true;
 		createHome = true;
-		#hashedPasswordFile = config.sops.secrets."user/user".path;
+		hashedPasswordFile = config.sops.secrets."user/user".path;
 		description = userName;
 		shell = pkgs.zsh;
 		extraGroups = [ "networkmanager" "wheel" ];
 		packages = with pkgs; [ ];
 	};
 
-	#users.users.root.hashedPasswordFile = config.sops.secrets."user/root".path;
+	users.users.root.hashedPasswordFile = config.sops.secrets."user/root".path;
 
-	#users.mutableUsers = false;
+	users.mutableUsers = false;
 
 }
