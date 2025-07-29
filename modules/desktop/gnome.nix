7012f154cfd39ca180f5e185b26f498852689ae3
1u5t1n14n <177038973+1u5t1n14n@users.noreply.github.com>
2025-07-29 15:32:41 +0200

    Deactivate Installation of all GNOME Core Apps so I can install them manually

--- modules/desktop/gnome.nix
+++ modules/desktop/gnome.nix
@@ -1,7 +1,16 @@
-{ pkgs, config, ... }:
+{ lib, pkgs, config, ... }:
 
 let
 	cfg = config.services.desktopManager.gnome;
+
+	mimeAppsList = pkgs.writeTextFile {
+		name = "gnome-mimeapps";
+		destination = "/share/applications/mimeapps.list";
+		text = ''
+			[Default Applications]
+			inode/directory=nautilus.desktop;org.gnome.Nautilus.desktop
+		'';
+	};
 
 in
 {
@@ -13,8 +22,16 @@ in
 		};
 		desktopManager.gnome.enable = true;
 
-		# Calendar Server
-		gnome.evolution-data-server.enable = true;
+		gnome = {
+			# Calendar Server
+			evolution-data-server.enable = true;
+
+			# Previewer for Nautilus
+			sushi.enable = true;
+
+			# Core Apps not installing by default
+			core-apps.enable = false;
+		};
 
 		# Disable XTerm
 		xserver.excludePackages = [ pkgs.xterm ];
@@ -27,21 +44,50 @@ in
 			package = pkgs.gnomeExtensions.gsconnect;
 		};
 
+		# Disable GNOME Console Integrations
+		# Since it is no longer installed
+		bash.vteIntegration = false;
+		zsh.vteIntegration = false;
+
 		nautilus-open-any-terminal = {
 			enable = true;
 			terminal = "wezterm";
 		};
 
-		# Disable GNOME Console Integrations
-		# Since it is no longer installed
-		bash.vteIntegration = false;
-		zsh.vteIntegration = false;
+		evince.enable = true;
+		file-roller.enable = true;
+		gnome-disks.enable = true;
 	};
 
 	environment = {
 		# Install some extra Applications
-		# And GNOME Shell Extensions
 		systemPackages = with pkgs; [
+			# Core Apps
+			baobab
+			decibels
+			epiphany
+			gnome-text-editor
+			gnome-calculator
+			gnome-calendar
+			gnome-characters
+			gnome-contacts
+			gnome-font-viewer
+			gnome-maps
+			loupe
+			nautilus
+
+			# Extra Apps
+			citations
+			morewaita-icon-theme
+			papers
+			pdfarranger
+		]
+
+		# Nautilus Thumbnailer
+		++ [ libheif libheif.out ]
+
+		# And GNOME Shell Extensions
+		++ lib.optionals cfg.enable [
 			gnomeExtensions.launch-new-instance
 			gnomeExtensions.blur-my-shell
 			gnomeExtensions.native-window-placement
@@ -50,42 +96,17 @@ in
 			gnomeExtensions.media-controls
 			gnomeExtensions.unite
 			gnomeExtensions.hide-activities-button
-
-			citations
-			impression
-			morewaita-icon-theme
-			papers
-			pdfarranger
 		]
 
 		++ lib.optionals (config.programs.nautilus-open-any-terminal.terminal == "wezterm" ) [ wezterm ];
 
-		# Exclude Packages that would be installed
-		# For GNOME
-		gnome.excludePackages = with pkgs; [
-			# E-Mail Application
-			geary
-			gnome-clocks
-			gnome-connections
-			gnome-console
-			gnome-font-viewer
-			gnome-logs
-			gnome-music
-			gnome-system-monitor
-			gnome-tour
-			gnome-weather
-
-			# Screen Reader
-			orca
-			seahorse
-
-			# Camera Tool
-			snapshot
-			totem
-			yelp
-		];
+		gnome.excludePackages = [ pkgs.gnome-tour ];
 
 		sessionVariables = {
+			# Nautilus
+			NAUTILUS_4_EXTENSION_DIR = lib.mkIf cfg.enable "${config.system.path}/lib/nautilus/extensions-4";
+			XDG_DATA_DIRS = lib.mkIf cfg.enable [ "${mimeAppsList}/share" ];
+
 			# Hint Electron Apps to use Wayland
 			NIXOS_OZONE_WL = "1";
 
@@ -94,7 +115,29 @@ in
 			QT_QPA_PLATFORMTHEME = "gtk2";
 			QT_QPA_PLATFORM = "wayland";
 		};
+
+		# Nautilus
+		pathsToLink = [
+			"/share/nautilus-python/extensions"
+
+			# Thumbnailers
+			"share/thumbnailers"
+		];
 	};
+
+	# GStreamer Plugins for Nautilus
+	nixpkgs.overlays = [
+		(final: prev: {
+			nautilus = prev.nautilus.overrideAttrs (nprev: {
+				buildInputs =
+					nprev.buildInputs
+					++ (with pkgs.gst_all_1; [
+						gst-plugins-good
+						gst-plugins-bad
+					]);
+			});
+		})
+	];
 
 	# Automatic Screen Rotation
 	hardware.sensor.iio.enable = true;
