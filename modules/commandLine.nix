43a605836e6be2b1a2518f07bedc009f87188b4d
1u5t1n14n <177038973+1u5t1n14n@users.noreply.github.com>
2025-09-05 11:19:19 +0200

    Add GnuPG Support

--- modules/commandLine.nix
+++ modules/commandLine.nix
@@ -20,14 +20,22 @@
 
 		nano.enable = true;
 		git.enable = true;
+
+		gnupg.agent = {
+			enable = true;
+			pinentryPackage = pkgs.pinentry-curses;
+			enableSSHSupport = true;
+		};
 	};
+
+	services.pcscd.enable = true;
 
 	environment.systemPackages = with pkgs; [
 		# Common CLIs
 		ripgrep fastfetch onefetch tree gcc tree-sitter
 		fastfetch fzf figlet zip unzip wget yt-dlp btop
 		gh cmatrix browsh imagemagick superfile gnumake
-		gnupg pandoc typst texliveFull bitwarden-cli
+		pandoc typst texliveFull bitwarden-cli gnupg
 
 		# FFmpeg
 		(pkgs.ffmpeg-full.override { withUnfree = true; })
