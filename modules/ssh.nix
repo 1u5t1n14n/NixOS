ca563b0e7fffe3ad5ac5873e084dbec3a4327378
1u5t1n14n <177038973+1u5t1n14n@users.noreply.github.com>
2025-09-05 11:19:25 +0200

    Add SSH Keys

--- modules/ssh.nix
+++ modules/ssh.nix
@@ -1,9 +1,11 @@
-{ host, lib, config, ... }:
+{ host, config, ... }:
 
 {
 
 	users.users.${host.user}.openssh.authorizedKeys.keys = [
-		"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJEwFDVhbXfV9zM8iQurlgoCo0lpIMBuJ1R7TS+tcYT1"
+		"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJEwFDVhbXfV9zM8iQurlgoCo0lpIMBuJ1R7TS+tcYT1 user@Termius"
+		"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFP9kyiHSaLuGocie+qs2a5jXRRzsOpruo2P+Bq5j4fS user@Hyperion"
+		"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINduKkKYcK2ef0B7n63abU009TjGYdVC+Oqmtbsdflks user@Prometheus"
 	];
 
 	services = {
