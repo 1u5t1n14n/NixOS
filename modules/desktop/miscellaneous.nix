8d9ae313fddbdb6e083fa1f7610e3d4fb13e7854
1u5t1n14n <177038973+1u5t1n14n@users.noreply.github.com>
2025-09-29 16:38:29 +0200

    Add Anki

--- modules/desktop/miscellaneous.nix
+++ modules/desktop/miscellaneous.nix
@@ -5,7 +5,7 @@
 	environment.systemPackages = with pkgs; [
 		inkscape geogebra6 obsidian zed-editor
 		jetbrains.clion xorg.xprop keepassxc
-		wezterm
+		wezterm anki-bin
 	];
 
 	xdg.terminal-exec = {
