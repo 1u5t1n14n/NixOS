33205521a5331ec37f71c0a32d196f91602149d0
1u5t1n14n <177038973+1u5t1n14n@users.noreply.github.com>
2025-10-01 12:23:25 +0200

    PCI Compliance

--- modules/packages.nix
+++ modules/packages.nix
@@ -2,12 +2,17 @@
 
 {
 
-	environment.etc."nixPkgs".text =
-		let
-			packages = builtins.map (p: "${p.name}") config.environment.systemPackages;
-			sortedUnique = builtins.sort builtins.lessThan (pkgs.lib.lists.unique packages);
-			formatted = builtins.concatStringsSep "\n" sortedUnique;
-		in
-			formatted;
+	environment = {
+		etc.nixPkgs.text =
+			let
+				packages = builtins.map (p: "${p.name}") config.environment.systemPackages;
+				sortedUnique = builtins.sort builtins.lessThan (pkgs.lib.lists.unique packages);
+				formatted = builtins.concatStringsSep "\n" sortedUnique;
+			in
+				formatted;
+
+		# PCI Compliance
+		# systemPackages = [ clamav ];
+	};
 
 }
