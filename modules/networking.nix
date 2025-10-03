59fdae7fdf78993dcde8884a0d27c61bd718a3fc
1u5t1n14n <177038973+1u5t1n14n@users.noreply.github.com>
2025-10-03 18:48:47 +0200

    Make Wifi-MGMT declarative

--- modules/networking.nix
+++ modules/networking.nix
@@ -1,5 +1,24 @@
 { host, config, ... }:
 
+let
+	mkWifiConfig = ssid: {
+		connection = {
+			type = "wifi";
+			id = ssid;
+			permissions = "";
+		};
+		wifi = {
+			ssid = ssid;
+			mode = "infrastructure";
+		};
+		wifi-security = {
+			psk = "$" + ssid;
+			key-mgmt = "wpa-psk";
+		};
+		ipv4.method = "auto";
+	};
+
+in
 {
 
 	networking = {
@@ -8,7 +27,48 @@
 		firewall.enable = true;
 		nftables.enable = config.networking.firewall.enable;
 
-		networkmanager.enable = true;
+		networkmanager = {
+			enable = !config.networking.wireless.enable;
+			ensureProfiles = {
+				environmentFiles = [ config.sops.templates.wifi.path ];
+
+				profiles = {
+					trautesheim = mkWifiConfig "trautesheim";
+					kant = mkWifiConfig "kant";
+					Paulina = mkWifiConfig "Paulina";
+					Pixel = mkWifiConfig "Pixel";
+				};
+			};
+		};
+
+		wireless = {
+			enable = false;
+			secretsFile = config.sops.templates.wifi.path;
+			networks = {
+				Paulina.psk = "ext:paulina";
+				Pixel.psk = "ext:pixel";
+				kant.psk = "ext:kant";
+				trautesheim.psk = "ext:trautesheim";
+			};
+		};
+	};
+
+	sops = {
+		secrets = {
+			"networking/paulina" = { };
+			"networking/pixel" = { };
+			"networking/kant" = { };
+			"networking/trautesheim" = { };
+		};
+		templates.wifi = {
+			owner = "root";
+			content = ''
+				paulina=${config.sops.placeholder."networking/paulina"}
+				pixel=${config.sops.placeholder."networking/pixel"}
+				kant=${config.sops.placeholder."networking/kant"}
+				trautesheim=${config.sops.placeholder."networking/trautesheim"}
+			'';
+		};
 	};
 
 }
