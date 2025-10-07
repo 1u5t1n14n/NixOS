b2e0e2028a5b9a076a359fa9bd0918ca992eda54
1u5t1n14n <177038973+1u5t1n14n@users.noreply.github.com>
2025-10-07 17:54:25 +0200

    Correct GTK Settings

--- home/gtk.nix
+++ home/gtk.nix
@@ -25,13 +25,13 @@ in {
 			package = pkgs.morewaita-icon-theme;
 		};
 
-		gtk3.extraConfig.Settings = ''
-			gtk-application-prefer-dak-theme=1
-		'';
+		gtk3.extraConfig = {
+			gtk-application-prefer-dark-theme = 1;
+		};
 
-		gtk4.extraConfig.Settings = ''
-			gtk-application-prefer-dark-theme=1
-		'';
+		gtk4.extraConfig = {
+			gtk-application-prefer-dark-theme = 1;
+		};
 	};
 
 	home.pointerCursor = {
