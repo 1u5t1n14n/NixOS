294a131718327eda79ae9f26fb3102219df11a38
1u5t1n14n <177038973+1u5t1n14n@users.noreply.github.com>
2025-10-21 12:10:51 +0200

    Follow Home-Manager Option Renaming

--- home/git.nix
+++ home/git.nix
@@ -4,8 +4,10 @@
 
 	programs.git = {
 		enable = true;
-		userName = host.user;
-		userEmail = "177038973+1u5t1n14n@users.noreply.github.com";
+		settings.user = {
+			name = host.user;
+			email = "177038973+1u5t1n14n@users.noreply.github.com";
+		};
 	};
 
 	programs.gh.enable = true;
