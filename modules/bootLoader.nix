179b869971c835050f7bf0c3276714eb323c87ad
1u5t1n14n <177038973+1u5t1n14n@users.noreply.github.com>
2025-12-01 18:28:19 +0100

    Enable LUKS Splash Screen

--- modules/bootLoader.nix
+++ modules/bootLoader.nix
@@ -1,13 +1,42 @@
-{ ... }:
+{ pkgs, config, ... }:
 
+let
+	cfg = config.boot.plymouth;
+
+in
 {
 
 	boot = {
-		plymouth.enable = true;
-		kernelParams = [ "quiet" "random.trust_cpu=off" ];
+		consoleLogLevel = 3;
+		initrd = {
+			verbose = false;
+		};
+
+		kernelParams = [
+			"quiet"
+			"splash"
+			"boot.shell_on_fail"
+			"udev.log_priority=3"
+			"rd.systemd.show_status=auto"
+
+			"random.trust_cpu=off"
+		];
+
 		loader = {
+			timeout = 0;
 			systemd-boot.enable = true;
 			efi.canTouchEfiVariables = true;
+		};
+
+		plymouth = {
+			enable = true;
+			theme = "sphere";
+			themePackages = with pkgs; [
+				(adi1090x-plymouth-themes.override {
+					selected_themes = [ cfg.theme ];
+				})
+			];
+			font = "${pkgs.inter}/share/fonts/truetype/InterVariable.ttf";
 		};
 	};
 
